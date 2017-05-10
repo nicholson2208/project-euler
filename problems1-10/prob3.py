bf59d63502dfd66ee40b858850bb6a16aff61f89
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2017-05-10 13:13:48 -0500

    solved problem 3

--- problems1-10/prob3.py
+++ problems1-10/prob3.py
@@ -11,16 +11,12 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 
-og_n=81*4
+og_n=600851475143
 n=og_n
-for i in range(2,15): #note that this doesn't work if a factor is divisible by a square I think
-    if n ==1: break
-    if n%i==0:
-        print("cool, "+str(i)+" divides "+str(og_n)+", now make n the other factor "+str(n/i))
+
+#I think I basically need to reset the index of the loop each time
+for i in range(2,50000): #note that this doesn't work if a factor is divisible by a square I think
+    while n%i==0 and n != i:
         n=n/i
-        for ii in range(2,i):
-            if n%ii==0:
-                print("cool, "+str(ii)+" divides "+str(og_n)+", now make n the other factor "+str(n/ii))
-                n=n/ii
-        
-print(n)
+            
+print("the largest prime factor of "+str(og_n)+" is "+str(int(n)))
