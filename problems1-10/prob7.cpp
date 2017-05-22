ad11ccd91a7cdd38058a2e991dccfddc55be4ed0
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2017-05-22 00:21:49 -0500

    solved cpp prob7

--- problems1-10/prob7.cpp
+++ problems1-10/prob7.cpp
@@ -2,9 +2,31 @@ using namespace std;
 
 #include <iostream>
 
-int main(){
+long findNthPrime(int n);
 
+int main(){
+  findNthPrime(10001);
+
+  return 0;
 }
 
-long findNthPrime(){
+long findNthPrime(int n){
+  int number_primes = 0;
+
+  for(long number=2; number < 10000000; number++){
+    for(int i=2; i<number+1; i++){
+      if(number%i==0){
+	if(number != i){
+	  break;
+	} else {
+	  number_primes++;
+	  cout << "Found prime number " << number_primes << "! : " << number << endl;
+	  if(number_primes==n){
+	    return number;
+	  }
+	}
+      }
+    }
+  }
+
 }
