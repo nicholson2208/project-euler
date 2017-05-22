73b835403ae21250007e55dc9c6113c663e46621
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2017-05-22 15:35:28 -0500

    runs, product doesnt update

--- problems1-10/prob8.cpp
+++ problems1-10/prob8.cpp
@@ -4,11 +4,12 @@ using namespace std;
 #include <string>
 
 
-sting maxAdjProd(long num, int run_length);
-int multListElements(int, int size);
+string maxAdjProd(long num, int run_length);
+int multListElements(int arr[], int size);
 
 int main(){
-  string best_string = maxAdjProd(1111000,4);
+  //lol I think this number is too big 
+  string best_string = maxAdjProd(30420752963450,4);
   cout<<"the best is "<< best_string<<endl;
   return 0;
 }
@@ -20,8 +21,9 @@ string maxAdjProd(long num, int run_length){
   int best_nums[run_length];
 
 
-  for(int i =0; i<best_nums.size(); i++){
-    all_nums[i]=stoi(s[i]);
+  for(int i =0; i<s.length(); i++){
+    all_nums[i]=(int)s[i]-48; //something about ascii
+    cout << all_nums[i] << endl;
   }
 
   int iter=0;
@@ -30,7 +32,7 @@ string maxAdjProd(long num, int run_length){
 
   while(iter< s.length()-run_length+1){
     int nums[run_length];
-
+    cout << "the iter is "<< iter << endl;
     for(int ii=0; ii<run_length; ii++){
       nums[ii]=all_nums[ii+iter];
     }
@@ -39,6 +41,7 @@ string maxAdjProd(long num, int run_length){
     int product = multListElements(nums, run_length);
 
     if(product > max_product){
+      cout << "product is now "<<product << "and max is "<<max_product << endl;
       max_product=product;
       for(int i=0; i< run_length; i++){ //idk if this part works
 	best+=to_string(nums[i])+" ";
@@ -51,11 +54,11 @@ string maxAdjProd(long num, int run_length){
 }
 
 int multListElements(int arr[], int size){ //have a helper variable to make life ez
-
+  cout <<"the first element of nums is " << arr[0] <<"and size is " << size<< endl;
   int product=1;
   for(int i; i<size; i++){
-    product*=arr[i];
+    product = product  * arr[i];
   }
-
+  cout << "the product I am returning is "<< product << endl;
   return product;
 }
