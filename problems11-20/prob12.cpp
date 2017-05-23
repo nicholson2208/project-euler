78c493233d79acce7ae2576de379cab6b1228b73
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2017-05-23 14:52:49 -0500

    so close on 12

--- problems11-20/prob12.cpp
+++ problems11-20/prob12.cpp
@@ -1,6 +1,7 @@
 using namespace std;
 
 #include <iostream>
+#include <map>
 
 int numTriFactors(int num_divisors);
 
@@ -13,7 +14,50 @@ int main(){
 
 int numTriFactors(int num_divisors){
   //maybe create a dictionary to store all of the factors
+  map<int, int> factors;
+  int jump=2;
+  int number=3;
+  int old_num=1;
 
+  factors.insert(pair<int,int>(1,1)); // initialize with 1
+  while(factors.find(old_num)->second < num_divisors && old_num<28)
+    { //this might be leq instead
+    //basically go through the loop of numbers and find factors
+    // but if your come across a factor you have already seen, 
+    //look it up instead of recalculating it
+
+
+    int num_factors =0;
+    int smallest_big_factor=number;
+    for(int i=1; i<smallest_big_factor; i++)
+      {
+	int curr = number;
+	if(curr%i==0 && curr!=i)
+	  { //you have found a factor pair
+	    curr=curr/i; //so now curr and i are the factors
+	    smallest_big_factor=curr;
+
+	    cout << "factor pair: " << curr <<" "<< i << endl;
+	    if(factors.find(i) != factors.end())
+	      { //you have a workable subproblem
+		num_factors=num_factors+factors.find(curr)->second + factors.find(i)->second;
+		cout<<"suh dude i'm insertin" << endl;
+		factors.insert(pair<int,int>(number,num_factors));
+		break; 
+	  
+	      } else
+	      { //you don't know how many factors curr has
+	 
+		num_factors=num_factors+2;
+	      }
+	  }
+	}
+    cout <<"num factors: "<< factors.find(number)->second << endl;
+    jump++;
+    old_num =number;
+   number=number+jump;
+   cout << number << endl;
+   }
   
-  return number;
+  return old_num;
 }
