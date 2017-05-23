d4fe61cc5599e0c98fad354e67cd738999b481a3
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2017-05-23 00:34:40 -0500

    added p12

---
+++ problems11-20/prob12.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+
+#include <iostream>
+
+int numTriFactors(int num_divisors);
+
+int main(){
+  int n= 6;
+
+  cout<< "The first triangle number with " << n <<" divisors is " << numTriFactors(n) << endl;
+  return 0;
+}
+
+int numTriFactors(int num_divisors){
+  //maybe create a dictionary to store all of the factors
+
+  
+  return number;
+}
